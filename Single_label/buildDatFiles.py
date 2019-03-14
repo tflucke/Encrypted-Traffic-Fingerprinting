010a0d640bf793ecdc6e3dff24ffe761dbf2d00d
Thomas Flucke <tomflucke@gmail.com>
2019-03-13 17:26:20 -0700

    Added code to pad traces.

--- Single_label/buildDatFiles.py
+++ Single_label/buildDatFiles.py
@@ -2,6 +2,23 @@
 
 import feature_extraction as fe
 import itertools
+from copy import copy
+
+MTU = 1500
+DATA_THRESHOLD = 100
+
+def padTraces(traces, smart=False):
+    paddedTraces = []
+    for t in traces:
+        pt = copy(t)
+        pt.packetsizes = []
+        for p in t.packetsizes:
+            if not smart or abs(p) >= DATA_THRESHOLD:
+                pt.packetsizes.append(MTU * abs(p) / p)
+            else:
+                pt.packetsizes.append(p)
+        paddedTraces.append(pt)
+    return paddedTraces
 
 if __name__ == "__main__":
     allTraces = fe.window_all_traces(fe.load_traces())
@@ -21,3 +38,11 @@ if __name__ == "__main__":
         narrowedTraces.extend(list(traces)[0:maxLen])
     print("Saving %d traces to narrowed_traces.dat" % len(narrowedTraces))
     fe.pickle_traces(narrowedTraces, "tor_traces/narrowed_traces.dat")
+    print("Padding all traces...")
+    paddedTraces = padTraces(allTraces)
+    print("Saving %d padded traces to padded_traces.dat" % len(paddedTraces))
+    fe.pickle_traces(paddedTraces, "tor_traces/padded_traces.dat")
+    print("Smart padding all traces...")
+    smartPaddedTraces = padTraces(allTraces, True)
+    print("Saving %d padded traces to smart_traces.dat" % len(smartPaddedTraces))
+    fe.pickle_traces(paddedTraces, "tor_traces/smart_traces.dat")
