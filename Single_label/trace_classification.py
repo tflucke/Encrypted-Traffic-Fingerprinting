561ab7c156ce1987a274a799ccc76c1d9bdc7179
aron <amotis@calpoly.edu>
2019-03-13 22:57:48 -0700

    Updated to use argparse for better argument parsing and modified graphs.

--- Single_label/trace_classification.py
+++ Single_label/trace_classification.py
@@ -2,6 +2,7 @@ from trace import *
 from feature_extraction import *
 import numpy as np
 import sys
+import argparse as ap
 from time import strftime
 from scipy.sparse import csr_matrix, vstack
 from sklearn.naive_bayes import MultinomialNB
@@ -48,11 +49,29 @@ def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blu
     plt.xlabel('Predicted label')
 
 if __name__ == "__main__":
-    mode = sys.argv[1]
-    FEATURE = sys.argv[2] # use burst features or size_IAT ('size_IAT', 'burst' or 'both')
-    METHOD = sys.argv[3] # options: 'NB' : Naive Bayes, 'RF' : random forest, 'MLP' : , 'LR': logistic regression
 
-    if mode == 'unencr':
+    parser = ap.ArgumentParser(description = "Create visualizations from data")
+
+    # Mode will almost always be Tor.
+    parser.add_argument('mode', metavar = 'mode', type = str,
+                        action = 'store', help = 'The mode to use. Usally "tor".')
+
+    # Feature may be burst, size_IAT, or both.
+    parser.add_argument('feature', metavar = 'feature', type = str,
+                        action = 'store', help = 'The feature to use. "burst", "size_IAT", or "both"')
+
+    # Method may be NB (Naive Bayes), RF (random forests),
+    # LR (logistic regression), or MLP (not explained so I don't know).
+    parser.add_argument('method', metavar = 'method', type = str,
+                        action = 'store', help = 'The method to use. "NB" for Naive Bayes, "LR" for logistic regression, "RF" for random forests, and MLP for ???')
+
+    # Optional option to specify the trace file to use.
+    parser.add_argument('--trace', dest = 'trace_file', help = 'The trace file to load')
+
+    # Parse arguments.
+    args = parser.parse_args()
+
+    if args.mode == 'unencr':
         parameters = {'size_IAT' : 
             {'NB':{'alpha': 0.1}, 
              'RF': {'n_estimators': 11},
@@ -68,7 +87,7 @@ if __name__ == "__main__":
              'RF': {'n_estimators': 21},
              'MLP': {'alpha': 0.0001, 'max_iter' : 300, 'hidden_layer_sizes' : (100,)},
              'LR': {'C': 10000, 'tol': 0.01}}}
-    elif mode == 'ipsec_ns':
+    elif args.mode == 'ipsec_ns':
         parameters = {'size_IAT' : 
             {'NB':{'alpha': 1.0000000000000001e-05}, 
              'RF': {'n_estimators': 20},
@@ -84,7 +103,7 @@ if __name__ == "__main__":
              'RF': {'n_estimators': 13},
              'MLP': {'alpha': 0.01, 'max_iter' : 300, 'hidden_layer_sizes' : (100,100)},
              'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_def':
+    elif args.mode =='ipsec_def':
         parameters = {'size_IAT' : 
             {'NB':{'alpha': 0.01}, 
              'RF': {'n_estimators': 24},
@@ -100,91 +119,91 @@ if __name__ == "__main__":
              'RF': {'n_estimators': 20},
              'MLP': {'alpha': 0.01, 'max_iter' : 300, 'hidden_layer_sizes' : (100,100)},
              'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_20ps':
+    elif args.mode =='ipsec_20ps':
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_50ps':
+    elif args.mode =='ipsec_50ps':
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_100ps':
+    elif args.mode =='ipsec_100ps':
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 1000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_150ps':  
+    elif args.mode =='ipsec_150ps':  
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 10000, 'tol': 0.01}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_200ps' : 
+    elif args.mode =='ipsec_200ps' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}} 
-    elif mode =='ipsec_300ps' : 
+    elif args.mode =='ipsec_300ps' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}} 
-    elif mode =='ipsec_400ps' : 
+    elif args.mode =='ipsec_400ps' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}}, # actually 100, but this is probably an error
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_20_ud' : 
+    elif args.mode =='ipsec_20_ud' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_50_ud' : 
+    elif args.mode =='ipsec_50_ud' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_100_ud' : 
+    elif args.mode =='ipsec_100_ud' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_150_ud' : 
+    elif args.mode =='ipsec_150_ud' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_200_ud' : 
+    elif args.mode =='ipsec_200_ud' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='ipsec_300_ud' : 
+    elif args.mode =='ipsec_300_ud' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}},
          'burst':
             {'LR': {'C': 100000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100, 'tol': 0.0001}}}
-    elif mode =='ipsec_400_ud' : 
+    elif args.mode =='ipsec_400_ud' : 
         parameters = {'size_IAT' : 
             {'LR': {'C': 100000, 'tol': 0.0001}}, # actually 100, but this is probably an error
          'burst':
             {'LR': {'C': 10000, 'tol': 0.0001}}, 
          'both': {'LR': {'C': 100000, 'tol': 0.0001}}}
-    elif mode =='tor':
+    elif args.mode =='tor':
         parameters = {'size_IAT' : 
             {'NB':{'alpha': 1.0000000000000001e-05}, 
              'RF': {'n_estimators': 22},
@@ -203,8 +222,17 @@ if __name__ == "__main__":
     else:
         sys.exit("Execute as: python trace_classification.py 'mode' \n mode = " + str(modes))
     
-    all_traces = load_pickled_traces(load_mode=mode)
-    #windowed_traces = window_all_traces(all_traces, window_size = 1024)
+    # Load traces from trace file.
+    if args.trace_file:
+        try:
+            all_traces = load_pickled_traces(load_mode=mode, o_file=args.trace_file)
+        except IOError:
+            sys.exit("Could not open trace file '{}'".format(args.trace_file))
+    else:
+        try:
+            all_traces = load_pickled_traces(load_mode=mode)
+        except IOError:
+            sys.exit("Could not open default trace file")
 
     # Split test set but keep windows from different traces seperated from eachother
     labels = [x.label for x in all_traces]
@@ -215,25 +243,25 @@ if __name__ == "__main__":
 
     #print X_train_val, y_train_val, X_test, y_test
     # Use parameters as obtained from CV_hyperparameters
-    if METHOD == 'NB':
+    if args.method == 'NB':
         clf = MultinomialNB()
-    elif METHOD == 'RF':
+    elif args.method == 'RF':
         clf = RandomForestClassifier()
-    elif METHOD == 'MLP':
+    elif args.method == 'MLP':
         clf = MLPClassifier(solver = 'sgd', learning_rate = 'adaptive', random_state = 0)
-    elif METHOD == 'LR':
+    elif args.method == 'LR':
         clf = LogisticRegression()
     
     # Set parameters
-    clf.set_params(**parameters[FEATURE][METHOD])
+    clf.set_params(**parameters[args.feature][args.method])
     
-    if FEATURE == 'size_IAT':
+    if args.feature == 'size_IAT':
         feature_matrix, classes, train_range = build_feature_matrix_size_IAT(X_train_val)
         feature_matrix_test, classes_test, test_range = build_feature_matrix_size_IAT(X_test, train_range)
-    elif FEATURE == 'burst':
+    elif args.feature == 'burst':
         feature_matrix, classes, train_range = build_feature_matrix_burst(X_train_val)
         feature_matrix_test, classes_test, test_range = build_feature_matrix_burst(X_test, train_range)
-    elif FEATURE == 'both':
+    elif args.feature == 'both':
         feature_matrix, classes, train_range = build_feature_matrix_both(X_train_val)
         feature_matrix_test, classes_test, test_range = build_feature_matrix_both(X_test, train_range)   
 
@@ -241,12 +269,23 @@ if __name__ == "__main__":
     
     print clf.score(feature_matrix_test, classes_test)
     
+    if args.trace_file:
+        trace_file = args.trace_file.split('/')[1][:-4]
+    else:
+        trace_file = 'default'
+
     # Compute confusion matrix
     cnf_matrix = confusion_matrix(classes_test, clf.predict(feature_matrix_test))
     np.set_printoptions(precision=2)
     
     # Plot normalized confusion matrix    
     plt.figure()
-    plot_confusion_matrix(cnf_matrix, classes=traffic_types, title='Normalized confusion matrix')
+    #title = 'Normalized confusion matrix for ' + trace_file + ' ' + args.
+    title = trace_file.replace('_', ' ') + ' ' + args.feature + ' using ' + args.method
+    plot_confusion_matrix(cnf_matrix, classes=traffic_types, title=title)
 
+    filename = "confusion_matrix_" + trace_file + '_'
+    filename += args.mode + '_' + args.feature + '_' + args.method + '.png'
+
+    plt.savefig(filename)
     plt.show()
